d04561354c75ebde82a9d96a02dcb58d7aa5c3dc
Latorre <andre.latorre91@gmail.com>
2026-01-16 09:41:00 -0300

    Implemented route for QR Code visualization

--- payments/pix.py
+++ payments/pix.py
@@ -6,7 +6,7 @@ class Pix:
 
     def create_payment(self):
         # Criar o pagamento na instituição financeira
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         #gerar o copia e cola
         hash_payment = f'hash_payment_{bank_payment_id}'
