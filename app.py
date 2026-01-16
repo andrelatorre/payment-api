730b80b1ed82674f3ac1223f6fcfa73bc66be0da
Latorre <andre.latorre91@gmail.com>
2026-01-16 12:44:42 -0300

    Customer notification, 404 page, and server disconnection

--- app.py
+++ app.py
@@ -61,12 +61,21 @@ def pix_confirmation():
 
     payment.paid = True
     db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
 
     return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if not payment:
+        return render_template('404.html')
+
+    if payment.paid:
+        return render_template('confirmed_payment.html',payment_id=payment.id, 
+                           value=payment.value, 
+                           host="http://127.0.0.1:5000")
 
     return render_template('payment.html', 
                            payment_id=payment.id, 
@@ -78,6 +87,10 @@ def payment_pix_page(payment_id):
 @socketio.on('connect')
 def handle_connect():
     print("Client connected to the server")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Client has disconnect to the server")    
 
 
 if __name__ == '__main__':
