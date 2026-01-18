fa2c8426d4246a0392ccd7228c632b3fc97268e8
Latorre <andre.latorre91@gmail.com>
2026-01-18 17:31:05 -0300

    Creating unit tests for the Pix class

--- payments/pix.py
+++ payments/pix.py
@@ -4,7 +4,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # Criar o pagamento na instituição financeira
         bank_payment_id = str(uuid.uuid4())
 
@@ -13,7 +13,7 @@ class Pix:
 
         #gerar o qr_code
         img = qrcode.make(hash_payment)        
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png") #salva imagem no repositorio
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png") #salva imagem no repositorio
 
         return {"bank_payment_id": bank_payment_id,
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
